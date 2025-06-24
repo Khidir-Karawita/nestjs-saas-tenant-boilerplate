c38ad439f868f38999e89a4f6c413b70a6e71cb6
EL MAHDI <mehdibouzkoura1@gmail.com>
2025-06-24 23:13:10 +0100

    :adhesive_bandage: fix: update CHECK_POLICIES_KEY constant for consistency

--- src/common/decorators/metadata/check-policy.decorator.ts
+++ src/common/decorators/metadata/check-policy.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
 import { PolicyHandler } from 'src/common/interfaces/policy-handler.interface';
 
-export const CHECK_POLICIES_KEY = 'check_policy';
+export const CHECK_POLICIES_KEY = 'checkPolicy';
 export const CheckPolicies = (...handlers: PolicyHandler[]) =>
   SetMetadata(CHECK_POLICIES_KEY, handlers);
