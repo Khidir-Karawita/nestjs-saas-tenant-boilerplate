efda02a4482b0612daf638cc48c14e280b95ebc9
EL MAHDI <mehdibouzkoura1@gmail.com>
2025-06-25 19:15:00 +0100

    :sparkles: feat: add SkipTenant decorator and update TenantInterceptor

--- src/tenants/tenants.interceptor.ts
+++ src/tenants/tenants.interceptor.ts
@@ -13,6 +13,7 @@ import { ConfigType } from '@nestjs/config';
 import tenantConfig from 'src/config/tenant.config';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from 'src/common/decorators/metadata/auth.decorator';
+import { SKIP_TENANT_KEY } from 'src/common/decorators/metadata/skip-tenant.decorator';
 
 @Injectable()
 export class TenantInterceptor implements NestInterceptor {
@@ -28,9 +29,17 @@ export class TenantInterceptor implements NestInterceptor {
       context.getHandler(),
       context.getClass(),
     ]);
-    const { user } = request;
 
     if (isPublic) return next.handle();
+
+    const shouldSkipTenant = this.reflector.getAllAndOverride<boolean>(
+      SKIP_TENANT_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (shouldSkipTenant) return next.handle();
+
+    const { user } = request;
+
     if (!user) throw new InternalServerErrorException('User not found');
     const tenant = user.tenant as Tenant;
     if (!tenant) throw new InternalServerErrorException('Tenant not found');
